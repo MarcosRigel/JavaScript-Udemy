7851c2f1800c3cf60b4442552d429e57e92cd57e
Marcos Rigel <marcosrigel99@gmail.com>
2022-12-10 23:51:00 -0300

    concluindo a sessao de estrutura de programacao

---
+++ Secao 04 - Estrutura de programacao/1_variaveis.js
@@ -0,0 +1,24 @@
+let nome = "Matheus";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5; // declaracao
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+
+console.log(nome);
+
+laranja = 83284;
+
+console.log(laranja);
+
+laranja = "Laranja"; // atribuicao
+
+console.log(laranja);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
---
+++ Secao 04 - Estrutura de programacao/9_if.js
@@ -0,0 +1,21 @@
+let idade = 19;
+
+if (idade == 19) {
+    console.log("A idade é = 19");
+}
+
+if (idade > 25) {
+    console.log("A idade é maior que 25");
+}
+
+let nome = "Matheus";
+
+if (nome == "Matheus" && idade > 10) {
+    console.log("Liberado!");
+}
+
+let passaport = true;
+
+if ((nome == "Matheus" && idade > 10) || passaport == true) {
+    console.log("Liberado 2 !");
+}
